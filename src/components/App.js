195b8f77e38326efcc3a89c78a506c9e715b3c04
vishu-kr <vishwajeetku987@gmail.com>
2021-10-15 01:21:34 +0530

    creating app

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,24 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+		flag : false
+		}
 	};
+
+	myfunc() {
+		this.setState({
+			flag : true
+		})
+	}
 
     render() {
     	return(
     		<div id="main">
+				{this.state.flag && <p id ="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+				<button id ="click" onClick = {()=>this.myfunc() }>click</button>
 				{ /* Do not remove this main div!! */ }
+			
     		</div>
     	);
     }
